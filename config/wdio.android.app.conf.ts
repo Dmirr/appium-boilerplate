d5a120f13705744dfba10d9cd8664e2189b41b70
Dmirr <dmitriyrodionow@gmail.com>
2023-09-06 19:46:12 +0200

    HW25_1 done

--- config/wdio.android.app.conf.ts
+++ config/wdio.android.app.conf.ts
@@ -18,10 +18,10 @@ config.capabilities = [
         // The defaults you need to have in your config
         platformName: 'Android',
         maxInstances: 1,
-        // For W3C the appium capabilities need to have an extension prefix
+        // For W3Capapp the appium capabilities need to have an extension prefix
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'Pixel_3_10.0',
+        'appium:deviceName': 'Pixel 3 API 29',
         'appium:platformVersion': '10.0',
         'appium:orientation': 'PORTRAIT',
         'appium:automationName': 'UiAutomator2',
